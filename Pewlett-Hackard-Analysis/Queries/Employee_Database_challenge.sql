d6c7c8a75cd3d6ed99e07d163be2ff69ab767fbb
Matt Reinking <86776606+mattreinking@users.noreply.github.com>
2022-08-29 23:39:24 -0700

    Add files via upload

--- Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
+++ Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
@@ -1,10 +1,53 @@
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+-- Deliverable 1
 
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
+-- Retirement titles of employees who were born between January 1, 1952 and December 31, 1955
+SELECT e.emp_no,
+       e.first_name,
+       e.last_name,
+       t.title,
+       t.from_date,
+       t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+order by e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, title DESC;
+
+-- Retrieve # of employees by their most recent job title who are about to retire
+SELECT COUNT(ut.emp_no),
+ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title 
+ORDER BY COUNT(title) DESC;
+
+
+-- Deliverable 2
+
+-- Create Mentorship Eligibility table holding employees who are eligible to participate in a mentorship program
+SELECT DISTINCT ON(e.emp_no) e.emp_no, 
+    e.first_name, 
+    e.last_name, 
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+INTO mentorship_eligibilty
+FROM employees as e
+LEFT OUTER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+LEFT OUTER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01' AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
